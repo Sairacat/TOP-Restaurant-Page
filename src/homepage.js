81cac2d4f437a96056c803b898d89f26b36f826d
Sairacat <201769jzy@gmail.com>
2026-06-10 16:40:40 +0800

    Hard coded the template for the homepage. I do understand the logic behind the 'click the tab button then change the page content'. It's not that hard, but using javascript to create so many element and it's content is totally annoying to me. So, hardly did anything today, I shall look into it tomorrow.

---
+++ src/homepage.js
@@ -0,0 +1,8 @@
+const moduleArea = document.querySelector('.module');
+
+const homeTitle = document.createElement('div');
+homeTitle.classList.add('home-title');
+homeTitle.textContent = 'What is Little Witch Cafe?';
+
+const homeContent = document.createElement('div');
+homeContent.classList.add('home-content');
